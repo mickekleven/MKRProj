21093a2120ddd8a2c851b891eb0b84e35c44f5aa
Mikael Klevengård <mickekleven@outlook.com>
2022-07-06 16:47:01 +0200

    Add project files.

---
+++ MKRProj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MKRProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
